611df8204b63e22f0d4e1c09537b8ae9ddfbf177
oscar.savolainen <oscar.savolainen@thrivelearning.com>
2024-06-27 19:41:09 +0100

    Initial commit: added a main and a README for querying a remote server (assumed to be infinity server on Runpod instance) to get vectors from an embedding model.

---
+++ main.py
@@ -0,0 +1,66 @@
+import torch
+from torch import Tensor
+import requests
+from typing import List
+
+
+def get_server_embeddings(
+    server_address: str, text: List[str], model_name: str
+) -> Tensor:
+    """
+    Get embeddings from the infinity server.
+
+    Inputs:
+    - server_address (str): the address of the server running the model.
+    - text (List[str]): the text to get embeddings for.
+    - model_name (str): the name of the embedding model to get embeddings from.
+    """
+    data = {"input": text, "model": model_name}
+    response = requests.post(server_address, json=data)
+
+    resp_data = response.json()
+    embeddings = [
+        torch.Tensor([resp_data["data"][index]["embedding"]])
+        for index in range(len(resp_data["data"]))
+    ]
+    embeddings = torch.cat(embeddings)
+    return embeddings
+
+
+if __name__ == "__main__":
+
+    # Change this to your RUNPOD instance ID
+    RUNPOD_ID = "3ese5aumi826sg-644113f1"
+    # You can change this port as well, but make sure to run the infinity
+    # server on the same port, and expose that port on the rumpod instance
+    RUNPOD_INTERNAL_INF_PORT = 7997
+
+    # Server address for embeddings
+    server_address = (
+        f"https://{RUNPOD_ID}-{RUNPOD_INTERNAL_INF_PORT}.proxy.runpod.net/embeddings"
+    )
+    model_name = "BAAI/bge-m3"
+
+    # Input text to embed
+    input_texts = [
+        "hello world",
+        "salut, comment ca va?",
+        "todo bien!",
+    ]
+    print(f"We will embed these strings: {input_texts}")
+
+    # Make call to server to get embeddings
+    print("Getting embeddings from server...")
+    embeddings = get_server_embeddings(server_address, input_texts, model_name)
+
+    # Print results
+    print("First few values of each embedding:")
+    for index in range(len(input_texts)):
+        embedding = embeddings[index, :]
+        input_text = input_texts[index]
+
+        print(f"Text: {input_text}; Embedding: {embedding[:5]}")
+
+    print(f"The total embedding length is {len(embedding)}.")
+
+    print("SERIOUSLY:\nREMEMBER TO END THE RUNPOD INSTANCE ONCE YOU ARE DONE USING IT!!!!")
